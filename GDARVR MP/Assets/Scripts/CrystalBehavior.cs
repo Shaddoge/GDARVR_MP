690b5322f4be64610b8c89d1ff130ed213ad887a
John Santos <john_adriel_santos@dlsu.edu.ph>
2022-05-31 03:11:00 +0800

    Added crystal behavior script (WIP)

---
+++ GDARVR MP/Assets/Scripts/CrystalBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalBehavior : MonoBehaviour
+{
+    [SerializeField] private float heatTimeGoal = 3f;
+    private float heatedTime = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
