e6a14b052ddc01999b3a8526f81df5843195f46d
John Santos <john_adriel_santos@dlsu.edu.ph>
2022-05-31 03:02:43 +0800

    Added laser shader, and particle systems.

--- GDARVR MP/Assets/Scripts/LaserBehavior.cs
+++ GDARVR MP/Assets/Scripts/LaserBehavior.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(LineRenderer))]
 public class LaserBehavior : MonoBehaviour
 {
-    private LineRenderer lr;
+    [SerializeField] private LineRenderer laser;
+    [SerializeField] private GameObject hitParticle;
     [SerializeField] private int maxReflect = 3;
     [SerializeField] private Transform startPoint;
     [SerializeField] private bool reflectOnlyMirror;
 
     void Start()
     {
-        lr = GetComponent<LineRenderer>();
-        lr.positionCount = maxReflect + 1;
-        lr.SetPosition(0, startPoint.position);
+        laser.positionCount = maxReflect + 1;
+        laser.SetPosition(0, startPoint.position);
+        laser.enabled = true;
+        hitParticle.SetActive(false);
     }
 
     void Update()
@@ -24,8 +25,10 @@ public class LaserBehavior : MonoBehaviour
 
     private void CastLaser(Vector3 position, Vector3 direction)
     {
-        lr.SetPosition(0, startPoint.position);
+        int mirrorsHit = 0;
+        laser.SetPosition(0, startPoint.position);
 
+        //hitParticle.SetActive(false);
         for (int i = 0; i < maxReflect; i++)
         {
             Ray ray = new Ray(position, direction);
@@ -35,16 +38,31 @@ public class LaserBehavior : MonoBehaviour
             {
                 position = hit.point;
                 direction = Vector3.Reflect(direction, hit.normal);
-                lr.SetPosition(i + 1, hit.point);
-                
+                laser.SetPosition(i + 1, hit.point);
+
                 if (!hit.transform.CompareTag("Mirror") && reflectOnlyMirror)
                 {
                     for (int j = i + 1; j <= maxReflect; j++)
                     {
-                        lr.SetPosition(j, hit.point);
+                        laser.SetPosition(j, hit.point);
+                        if(!hitParticle.activeSelf)
+                            hitParticle.SetActive(true);
                     }
+                    hitParticle.transform.LookAt(hit.normal);
+                    hitParticle.transform.position = hit.point;
+                    //hitParticle.SetActive(true);
                     break;
                 }
+                else
+                {
+                    mirrorsHit++;
+                }
+            }
+            if(mirrorsHit == maxReflect)
+            {
+                hitParticle.transform.LookAt(hit.normal);
+                hitParticle.transform.position = hit.point;
+                //hitParticle.SetActive(true);
             }
         }
 
