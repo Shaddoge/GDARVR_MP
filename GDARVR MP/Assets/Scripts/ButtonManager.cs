3bf1cca3803bb12baa6ffaab2b10b9a54b83f8e9
Setiel B. Olivarez <setiel_b_olivarez@dlsu.edu.ph>
2022-06-01 04:53:11 +0800

    updates: - added level selection button and ui functionality - added scriptable components for levels - added lock and unlock level mechanics - added singleton managers (some are temp.) - added scene samples for testing note: levels must be added in project settings " Level <level number>" name convention

--- GDARVR MP/Assets/Scripts/ButtonManager.cs
+++ GDARVR MP/Assets/Scripts/ButtonManager.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class ButtonManager : MonoBehaviour
 {
-    Button button1;
+    public List<GameObject> LevelButtonList;
+    public string levelSceneNameToLoad;
+    [SerializeField] float buttonAlpha = 0.5f;
+    [SerializeField] int unlockedLevels = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //LevelButtonList = new List<GameObject>();
+        UpdateAvailableLevels(unlockedLevels);
+        UpdateAvailableButtons();
     }
 
     // Update is called once per frame
@@ -18,8 +25,62 @@ public class ButtonManager : MonoBehaviour
         
     }
 
-    public void TestFunc()
+    public void SelectThisLevel()
     {
-        
+        if (EventSystem.current.currentSelectedGameObject.CompareTag("LevelButton") == true)
+        {
+            LevelDetails levelDetails = EventSystem.current.currentSelectedGameObject.GetComponent <LevelDetails>();// get level details comp
+
+            levelSceneNameToLoad = levelDetails.level.SceneName;
+        }
     }
+    public void StartSelectedLevel()
+    {
+        Debug.Log("Current Selected Level: " + levelSceneNameToLoad);
+
+        SCENE_MANAGER.instance.LoadSelectedLevel(levelSceneNameToLoad);
+    }
+
+    public void UpdateAvailableLevels(int nUnlockedLevels)
+    {
+        Mathf.Clamp(nUnlockedLevels, 1.0f, LevelButtonList.Count);
+        for (int i = 0; i < nUnlockedLevels; i++)
+        {
+            LevelButtonList[i].GetComponent<LevelDetails>().level.isLocked = false;
+        }
+    }
+    public void UpdateAvailableButtons()
+    {
+        for (int i = 0; i < LevelButtonList.Count; i++)
+        {
+            LevelDetails levelDetails = LevelButtonList[i].GetComponent<LevelDetails>();
+            Button button = LevelButtonList[i].GetComponentInChildren<Button>();
+            Text text = LevelButtonList[i].GetComponentInChildren<Text>();
+            Color origColor = text.color;
+
+
+
+            if (levelDetails.level.isLocked == true)
+            {
+                Debug.Log(LevelButtonList[i].name + " locked");
+                button.interactable = false;
+                text.color = new Color(origColor.r, origColor.g, origColor.b, buttonAlpha);
+                text.text = "Locked";
+            }
+            else if (levelDetails.level.isLocked == false)
+            {
+                Debug.Log(LevelButtonList[i].name + " unlocked");
+                LevelButtonList[i].GetComponent<Button>().interactable = true;
+                text.color = new Color(origColor.r, origColor.g, origColor.b, 1.0f);
+                text.text = "LEVEL" +levelDetails.level.levelNumber;
+
+            }
+        }
+    }
+
+    
+
+
+    
+  
 }
---
+++ GDARVR MP/Assets/Scripts/Scriptable/Scripts/LevelDetails.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDetails : MonoBehaviour
+{
+    public Level level;
+    [SerializeField] string name;
+    [SerializeField] Sprite levelPreview;
+    [SerializeField] int levelNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
